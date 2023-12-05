ac78dd934c12d0b18e2ed4c1c3c259b012e9a007
Brian Perrin <perrin@live.com>
2023-12-04 16:34:42 -0800

    2018 Day 2 using a Trie
    The trie implementation is too specialized for this problem, but it's
    10x faster than the non-trie solution, and 5x faster than my simple trie
    solution, so I'll take it.

--- Year2018/Day2.cs
+++ Year2018/Day2.cs
@@ -2,7 +2,7 @@ namespace Moyba.AdventOfCode.Year2018
 {
     public class Day2(IEnumerable<string> data) : IPuzzle
     {
-        private readonly char[][] _data = data.Select(_ => _.ToCharArray()).ToArray();
+        private readonly string[] _data = data.ToArray();
 
         private int _checksum;
         private string? _prototypeID;
@@ -20,15 +20,10 @@ namespace Moyba.AdventOfCode.Year2018
 
             _checksum = doubleLetters * tripleLetters;
 
-            // PERF - swap to a trie?
-            var desiredLength = _data[0].Length - 1;
-            for (var first = 0; first < _data.Length && _prototypeID == null; first++)
+            var root = new TrieNode();
+            foreach (var id in _data)
             {
-                for (var second = first + 1; second < _data.Length && _prototypeID == null; second++)
-                {
-                    var commonLetters = _data[first].Zip(_data[second]).Where(_ => _.First == _.Second).ToArray();
-                    if (commonLetters.Length == desiredLength) _prototypeID = String.Join("", commonLetters.Select(_ => _.First));
-                }
+                if (root.ExistsOrAdd(id.ToCharArray(), out _prototypeID)) return Task.CompletedTask;
             }
 
             return Task.CompletedTask;
@@ -39,5 +34,59 @@ namespace Moyba.AdventOfCode.Year2018
 
         [Solution("lnfqdscwjyteorambzuchrgpx")]
         public string SolvePartTwo() => _prototypeID ?? String.Empty;
+
+        private class TrieNode(int index = -1, int _ignoreIndex = -1, char[]? _initial = null)
+        {
+            private readonly TrieNode[] _next = new TrieNode[27];
+            private readonly int _nextIndex = index + 1;
+
+            private bool _isInitialized;
+
+            public bool ExistsOrAdd(char[] characters, out string? value)
+            {
+                if (_nextIndex == characters.Length)
+                {
+                    char[] result = [ ..characters[.._ignoreIndex], ..characters[(_ignoreIndex+1)..] ];
+                    value = String.Join("", result);
+                    return true;
+                }
+
+                this.EnsureInitialized();
+
+                value = null;
+
+                var nextCharacter = characters[_nextIndex];
+                var characterIndex = _GetCharacterIndex(nextCharacter);
+
+                if (_ignoreIndex == -1)
+                {
+                    if (_next[0].ExistsOrAdd(characters, out value)) return true;
+                }
+
+                if (_next[characterIndex] == null) _next[characterIndex] = new TrieNode(_nextIndex, _ignoreIndex, characters);
+                else return _next[characterIndex].ExistsOrAdd(characters, out value);
+
+                return false;
+            }
+
+            private void EnsureInitialized()
+            {
+                if (_isInitialized) return;
+                _isInitialized = true;
+
+                if (_ignoreIndex == -1)
+                {
+                    _next[0] = new TrieNode(_nextIndex, _nextIndex, _initial);
+                }
+
+                if (_initial == null) return;
+
+                var nextCharacter = _initial[_nextIndex];
+                var characterIndex = _GetCharacterIndex(nextCharacter);
+                _next[characterIndex] = new TrieNode(_nextIndex, _ignoreIndex, _initial);
+            }
+
+            private static int _GetCharacterIndex(char c) => c - '`';
+        }
     }
 }
