5ec44ab123442f5fb862a5b8623159835f15230b
Brian Perrin <perrin@live.com>
2023-12-18 14:20:36 -0800

    2023 Day 18
    It's not pretty, but it's pretty fast. Frustratingly similar to Day 5 in
    approach, but different enough to cause trouble in the various cases
    where ranges collide.
    
    I probably should spend effort on a Coord library, and possibly a Range
    library. I seem to use them both a lot.

--- Year2023/Day18.cs
+++ Year2023/Day18.cs
@@ -1,7 +1,9 @@
 namespace Moyba.AdventOfCode.Year2023
 {
-    using Coord = (int x, int y);
-    using Instruction = (char direction, int distance, string color);
+    using Coord = (long x, long y);
+    using Range = (long start, long end);
+    using Instruction = ((long x, long y) direction, long distance);
+    using Plan = (char direction, long distance, string color);
 
     public class Day18(string[] _data) : IPuzzle
     {
@@ -9,7 +11,6 @@ namespace Moyba.AdventOfCode.Year2023
         private static readonly Coord _Down = (0, 1);
         private static readonly Coord _Left = (-1, 0);
         private static readonly Coord _Right = (1, 0);
-        private static readonly Coord[] _AllDirections = [ _Up, _Down, _Left, _Right ];
         private static readonly IDictionary<char, Coord> _directions = new Dictionary<char, Coord>
         {
             { 'U', _Up },
@@ -18,56 +19,223 @@ namespace Moyba.AdventOfCode.Year2023
             { 'R', _Right },
         };
 
-        private readonly Instruction[] _plan = _data
+        private readonly Plan[] _plan = _data
             .Select(_ => _.Split(' '))
-            .Select<string[], Instruction>(_ => (_[0][0], Int32.Parse(_[1]), _[2]))
+            .Select<string[], Plan>(_ => (_[0][0], Int64.Parse(_[1]), _[2]))
             .ToArray();
 
         [PartOne("92758")]
         [PartTwo()]
         public async IAsyncEnumerable<string?> ComputeAsync()
         {
-            Coord position = (0, 0);
-            var trench = new HashSet<Coord> { position };
-            foreach ((var direction, var distance, var color) in _plan)
-            {
-                var velocity = _directions[direction];
-                for (var iteration = 0; iteration < distance; iteration++)
-                {
-                    position = _ApplyVelocity(position, velocity);
-                    trench.Add(position);
-                }
-            }
+            yield return $"{_MeasureArea(_plan.Select(_ExtractInstructionFromPlan))}";
 
-            var minX = trench.Min(_ => _.x) - 1;
-            var maxX = trench.Max(_ => _.x) + 1;
-            var minY = trench.Min(_ => _.y) - 1;
-            var maxY = trench.Max(_ => _.y) + 1;
-            var outside = new HashSet<Coord>();
-            var queue = new Queue<Coord>();
-            queue.Enqueue((minX, minY));
-            while (queue.TryDequeue(out var coord))
-            {
-                if (outside.Contains(coord)) continue;
-                if (trench.Contains(coord)) continue;
-
-                outside.Add(coord);
-                foreach (var next in _AllDirections.Select(_ => _ApplyVelocity(coord, _)))
-                {
-                    if (next.x < minX || next.x > maxX || next.y < minY || next.y > maxY) continue;
-                    queue.Enqueue(next);
-                }
-            }
-
-            var area = (maxX - minX + 1) * (maxY - minY + 1) - outside.Count;
-
-            yield return $"{area}";
-
-            yield return $"";
+            yield return $"{_MeasureArea(_plan.Select(_ExtractInstructionFromColor))}";
 
             await Task.CompletedTask;
         }
 
-        private static Coord _ApplyVelocity(Coord position, Coord velocity) => (position.x + velocity.x, position.y + velocity.y);
+        private static long _MeasureArea(IEnumerable<Instruction> instructions)
+        {
+            var horizontals = new SortedList<long, SortedList<long, Range>>();
+
+            Coord position = (0L, 0L);
+            foreach ((var direction, var distance) in instructions)
+            {
+                var next = _ApplyDirection(position, direction, distance);
+                if (position.y == next.y)
+                {
+                    horizontals.TryAdd(position.y, new SortedList<long, Range>());
+
+                    Range range = (Math.Min(position.x, next.x), Math.Max(position.x, next.x));
+                    horizontals[position.y].Add(range.start, range);
+                }
+
+                position = next;
+            }
+
+            var current = new SortedList<long, long>();
+            var area = 0L;
+            var previousY = horizontals.GetKeyAtIndex(0);
+            for (var index = 0; index < horizontals.Count; index++)
+            {
+                var y = horizontals.GetKeyAtIndex(index);
+                area += (y - previousY) * current.Sum(_ => _.Value - _.Key + 1);
+                // _ResolveHorizontals(current, horizontals.GetValueAtIndex(index), ref area);
+                foreach (var horizontal in horizontals.GetValueAtIndex(index).Values) area += _ApplyRange(current, horizontal.start, horizontal.end);
+                previousY = y;
+            }
+
+            return area;
+        }
+
+        private static long _ApplyRange(SortedList<long, long> ranges, long start, long end)
+        {
+            var areaRemoved = 0L;
+
+            if (ranges.ContainsKey(start))
+            {
+                var newStart = Math.Min(end, ranges[start]);
+                var newEnd = Math.Max(end, ranges[start]);
+                ranges.Remove(start);
+
+                areaRemoved += newStart - start;
+                if (newStart == newEnd) return areaRemoved + 1;
+
+                start = newStart;
+                end = newEnd;
+            }
+
+            ranges.Add(start, end);
+
+            // can we collapse with the previous entry?
+            var index = ranges.IndexOfKey(start);
+            if (index > 0)
+            {
+                var previousStart = ranges.GetKeyAtIndex(index - 1);
+                var previousEnd = ranges.GetValueAtIndex(index - 1);
+                if (previousEnd == end)
+                {
+                    ranges[previousStart] = start;
+                    ranges.Remove(start);
+                    return areaRemoved + end - start;
+                }
+
+                if (previousEnd > end)
+                {
+                    ranges[previousStart] = start;
+                    ranges[end] = previousEnd;
+                    ranges.Remove(start);
+                    return areaRemoved + end - start - 1;
+                }
+
+                if (previousEnd == start)
+                {
+                    ranges.Remove(start);
+                    start = ranges.GetKeyAtIndex(index - 1);
+                    ranges[start] = end;
+                }
+            }
+
+            // do we overlap with the subsequent entries?
+            index = ranges.IndexOfKey(start);
+            while (index < ranges.Count - 1)
+            {
+                var nextStart = ranges.GetKeyAtIndex(index + 1);
+                if (nextStart > end) break;
+                if (nextStart == end)
+                {
+                    end = ranges[nextStart];
+                    ranges[start] = end;
+                    ranges.Remove(nextStart);
+                    break;
+                }
+
+                areaRemoved += ranges[nextStart] - nextStart - 1;
+                ranges[start] = nextStart;
+                start = ranges[nextStart];
+                ranges.Remove(nextStart);
+
+                if (start == end) return areaRemoved + 1;
+
+                ranges.Add(start, end);
+                index = ranges.IndexOfKey(start);
+            }
+
+            return areaRemoved;
+        }
+
+        private static void _ResolveHorizontals(SortedList<long, Range> ranges, SortedList<long, Range> horizontals, ref long area)
+        {
+            var rangeIndex = 0;
+            for (var horizontalIndex = 0; horizontalIndex < horizontals.Count; horizontalIndex++)
+            {
+                var horizontal = horizontals.GetValueAtIndex(horizontalIndex);
+
+                rangeIndex--;
+                Range? range;
+                do
+                {
+                    range = ++rangeIndex < ranges.Count ? ranges.GetValueAtIndex(rangeIndex) : null;
+                }
+                while (range != null && horizontal.start > range.Value.end);
+
+                if (range == null || horizontal.end < range.Value.start)
+                {
+                    ranges.Add(horizontal.start, horizontal);
+                    continue;
+                }
+
+                // overlap or adjacency in all following cases
+                ranges.RemoveAt(rangeIndex);
+
+                if (horizontal.end == range.Value.start)
+                {
+                    ranges.Add(horizontal.start, (horizontal.start, range.Value.end));
+                    continue;
+                }
+
+                if (horizontal.start == range.Value.end)
+                {
+                    ranges.Add(range.Value.start, (range.Value.start, horizontal.end));
+                    continue;
+                }
+
+                if (horizontal.start < range.Value.start) throw new Exception("Unexpected case #1.");
+
+                if (horizontal.start == range.Value.start)
+                {
+                    if (horizontal.end == range.Value.end)
+                    {
+                        area += horizontal.end - horizontal.start + 1;
+                        continue;
+                    }
+
+                    Range newRange = (Math.Min(horizontal.end, range.Value.end), Math.Max(horizontal.end, range.Value.end));
+                    ranges.Add(newRange.start, newRange);
+                    area += newRange.start - horizontal.start;
+                    continue;
+                }
+
+                if (horizontal.end == range.Value.end)
+                {
+                    Range newRange = (Math.Min(horizontal.start, range.Value.start), Math.Max(horizontal.start, range.Value.start));
+                    ranges.Add(newRange.start, newRange);
+                    area += horizontal.end - newRange.end;
+                    continue;
+                }
+
+                if (horizontal.end > range.Value.end) throw new Exception($"Unexpected case #2: ({horizontal.start}, {horizontal.end}) ({range.Value.start}, {range.Value.end})");
+
+                // one is inside the other, split into two
+                Range newRangeStart = (Math.Min(horizontal.start, range.Value.start), Math.Max(horizontal.start, range.Value.start));
+                ranges.Add(newRangeStart.start, newRangeStart);
+
+                Range newRangeEnd = (Math.Min(horizontal.end, range.Value.end), Math.Max(horizontal.end, range.Value.end));
+                ranges.Add(newRangeEnd.start, newRangeEnd);
+
+                area += newRangeEnd.start - newRangeStart.end - 1;
+            }
+        }
+
+        private static Instruction _ExtractInstructionFromPlan(Plan plan) => plan.direction switch
+        {
+            'U' => (_Up, plan.distance),
+            'D' => (_Down, plan.distance),
+            'L' => (_Left, plan.distance),
+            'R' => (_Right, plan.distance),
+            _ => throw new Exception($"Unexpected direction: {plan.direction}")
+        };
+
+        private static Instruction _ExtractInstructionFromColor(Plan plan) => plan.color[7] switch
+        {
+            '0' => (_Right, Int64.Parse(plan.color[2..7], System.Globalization.NumberStyles.HexNumber)),
+            '1' => (_Down, Int64.Parse(plan.color[2..7], System.Globalization.NumberStyles.HexNumber)),
+            '2' => (_Left, Int64.Parse(plan.color[2..7], System.Globalization.NumberStyles.HexNumber)),
+            '3' => (_Up, Int64.Parse(plan.color[2..7], System.Globalization.NumberStyles.HexNumber)),
+            _ => throw new Exception($"Unexpected color: {plan.color}")
+        };
+
+        private static Coord _ApplyDirection(Coord position, Coord direction, long distance) => (position.x + distance * direction.x, position.y + distance * direction.y);
     }
 }
