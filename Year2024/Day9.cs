f3490289d1ba608df0532d8ad2e3e54f8ea9872e
Brian Perrin <perrin@live.com>
2024-12-11 12:17:03 -0800

    2024 Day 9 perf As expected, building a lookup of free blocks by size allows the solution to run significantly faster. Now there are problems with overloaded naming (e.g. block vs. free block), but that's beyond the scope of an Advent of Code solution for which I expect minimal maintenance or readers.

--- Year2024/Day9.cs
+++ Year2024/Day9.cs
@@ -1,5 +1,8 @@
 namespace Moyba.AdventOfCode.Year2024
 {
+    using Disk = LinkedList<(int id, int length)>;
+    using Block = LinkedListNode<(int id, int length)>;
+
     public class Day9(string[] _data) : IPuzzle
     {
         private readonly int[] _diskMap = _data
@@ -31,35 +34,95 @@ namespace Moyba.AdventOfCode.Year2024
 
             yield return $"{checksum}";
 
-            var impossibleLength = 10;
-            var defrag = new LinkedList<(int id, int length)>(_diskMap.Select((_, i) => (_GetFileId(i), _)));
-            for (var block = defrag.Last; block != null; block = block.Previous)
+            var defrag = new Disk(_diskMap.Select((_, i) => (_GetFileId(i), _)));
+            checksum = _DefragAndChecksum(defrag);
+
+            yield return $"{checksum}";
+
+            await Task.CompletedTask;
+        }
+
+        private class FreeBlock
+        {
+            private readonly LinkedListNode<FreeBlock>[] _lookups = new LinkedListNode<FreeBlock>[9];
+
+            public FreeBlock(Block block, LinkedList<FreeBlock>[] freeBlocksByLength)
             {
-                if (block.Value.id == -1) continue;
-                if (block.Value.length >= impossibleLength) continue;
+                this.Block = block;
 
-                var foundFree = false;
-                for (var free = defrag.First; free != null && free != block; free = free.Next)
+                for (var i = 0; i < block.Value.length; i++)
                 {
-                    if (free.Value.id != -1) continue;
-                    if (free.Value.length < block.Value.length) continue;
-
-                    defrag.AddBefore(free, new LinkedListNode<(int id, int length)>(block.Value));
-                    block.Value = (-1, block.Value.length);
-
-                    if (free.Value.length == block.Value.length) defrag.Remove(free);
-                    else free.Value = (free.Value.id, free.Value.length - block.Value.length);
-
-                    foundFree = true;
-                    break;
+                    _lookups[i] = freeBlocksByLength[i].AddLast(this);
                 }
-
-                if (!foundFree) impossibleLength = block.Value.length;
             }
 
-            checksum = 0L;
+            public Block Block { get; }
+
+            public void Update()
+            {
+                for (var i = this.Block.Value.length; i < _lookups.Length; i++)
+                {
+                    var lookup = _lookups[i];
+                    if (lookup == null) break;
+
+                    lookup.List!.Remove(lookup);
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                    _lookups[i] = null;
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                }
+            }
+
+            public void MakeUnavailable()
+            {
+                for (var i = 0; i < _lookups.Length; i++)
+                {
+                    var lookup = _lookups[i];
+                    if (lookup == null) break;
+
+                    lookup.List!.Remove(lookup);
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                    _lookups[i] = null;
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                }
+            }
+        }
+
+        private static long _DefragAndChecksum(Disk disk)
+        {
+            var freeBlocksByLength = Enumerable.Range(0, 9).Select(_ => new LinkedList<FreeBlock>()).ToArray();
+            for (var block = disk.First; block != null; block = block.Next)
+            {
+                if (block.Value.id != -1) continue;
+
+                new FreeBlock(block, freeBlocksByLength);
+            }
+
+            for (var block = disk.Last; block != null; block = block.Previous)
+            {
+                if (block.Value.id == -1)
+                {
+                    if (block.Value.length > 0) freeBlocksByLength[0].Last!.Value.MakeUnavailable();
+                    continue;
+                }
+
+                var freeBlock = freeBlocksByLength[block.Value.length - 1].First?.Value;
+                if (freeBlock == null) continue;
+
+                disk.AddBefore(freeBlock.Block, new Block(block.Value));
+                block.Value = (-1, block.Value.length);
+
+                freeBlock.Block.Value = (freeBlock.Block.Value.id, freeBlock.Block.Value.length - block.Value.length);
+                freeBlock.Update();
+            }
+
+            return _CalculateChecksum(disk);
+        }
+
+        private static long _CalculateChecksum(Disk disk)
+        {
+            var checksum = 0L;
             var index = 0;
-            for (var node = defrag.First; node != null; node = node.Next)
+            for (var node = disk.First; node != null; node = node.Next)
             {
                 if (node.Value.id == -1)
                 {
@@ -73,9 +136,7 @@ namespace Moyba.AdventOfCode.Year2024
                 }
             }
 
-            yield return $"{checksum}";
-
-            await Task.CompletedTask;
+            return checksum;
         }
 
         private static int _GetFileId(int index)
